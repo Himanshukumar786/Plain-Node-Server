61b5583cbaec32259e25d0b3415e5c95b0bd9a0a
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-14 22:23:14 +0530

    Added code

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const http = require('http'); // Import Node.js core module
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    
+
+    console.log('Request received');
+    console.log(req);
+    console.log(res);
+
+    // send a hello world response
     res.write('Hello world'); // write a response to the client
 
     res.end(); // end the response
 });
 
+// start your server
 server.listen(PORT, () => {
-    console.log("Server listening on: http://localhost:", PORT)
+    // Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:", PORT);
 });
